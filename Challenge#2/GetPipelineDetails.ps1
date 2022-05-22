a5d31bf8423208ebf2a1fd8df183ad454d3153d7
RaviShankar1985 <72045632+RaviShankar1985@users.noreply.github.com>
2022-05-22 12:40:30 +0530

    Update GetPipelineDetails.ps1

--- Challenge#2/GetPipelineDetails.ps1
+++ Challenge#2/GetPipelineDetails.ps1
@@ -1,1 +1,11 @@
+$token = $Env:PAT
+$Auth = '{0}:{1}' -f '$user', $token
+$Auth = [System.Text.Encoding]::UTF8.GetBytes($Auth)
+$Auth = [System.Convert]::ToBase64String($Auth)
+$headers = @{Authorization = ("Basic {0}" -f $Auth)}
 
+$url = "https://dev.azure.com/ravi/_apis/distributedtask/pools/$poolid/jobrequests"
+
+$results = Invoke-RestMethod -Uri -Headers $headers -Method Get -ContentType "application/json"
+
+$results.value | Select-object requestid, poolid, result
